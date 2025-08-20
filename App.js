08998e9bcf77f97a0bbadd57e6272785dab31b27
Sbonelo2 <cbonelo224@gmail.com>
2025-08-20 11:18:31 +0200

    map,append

---
+++ App.js
@@ -0,0 +1,16 @@
+const numbers = [2, 67, 5, 18, 33]
+// console.log(conitaner);
+
+
+for (let i = 0; i < 5; i++){
+// console.log(i);
+}
+
+// numbers.map((element) => console.log(element * 2));
+
+const newCollection = numbers.map((element) => element * 2);
+console.log(newCollection)
+
+
+
+bone.append("this is a test")
